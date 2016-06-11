f18dfa197f759189e676725f3f246c0d93a71a82
Phong Phan <fongfan999@gmail.com>
2016-06-11 16:41:38 +0700

    Add some necessary Gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
+//= require flat-ui
+//= require flat-ui/flatui-checkbox
+//= require flat-ui/flatui-radio
