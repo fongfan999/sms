c6561cf9cbc884525dcdc56ce1e170f57084c150
Phan Thai Phong <fongfan999@gmail.com>
2016-07-12 17:46:34 +0700

    Validate Klass and Notification models

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -3,6 +3,9 @@ class Klass < ActiveRecord::Base
   has_many :students, dependent: :destroy
   belongs_to :teacher
 
+  validates :name, presence: true, length: { minimum: 3, maximum: 10 },
+    uniqueness: true
+
   def scores(semester)
   	ids = students.map(&:id)
   	Score.where("student_id IN (?) AND semester_id = ? ", ids, semester.id)
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,3 @@
 class Notification < ActiveRecord::Base
+  validates :title, :description, presence: true
 end
